b28003cb60bc5ab6d1062b27d06f7cc777f7179a
karenjunqueira <karencatigua@gmail.com>
2024-08-15 15:18:28 -0300

    feat: including websocket

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routers.item_router import router as item_router
+from routers.websocket_router import router as websocket_router
 
 app = FastAPI()
 app.include_router(item_router, 
     prefix="/item",
     tags=["Item"])
+app.include_router(websocket_router)
